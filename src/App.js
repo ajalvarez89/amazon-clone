72e2823055c026c79bffbb2c34853620a174a728
Alvaro Alvarez <alvaro.alvarez@monokera.com>
2020-09-10 13:49:27 -0500

    set routes into app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/home/Home';
+import Checkout from './components/Checkout/Checkout'
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //BEM
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path='/checkout'>
+            <Header />
+            <Checkout/>
+          </Route>
+          <Route path='/'>
+            <Header />
+            <Home />
+            <Footer />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBD0BSlwa10rasMMgeIdjIJqWgiudCefQA",
+  authDomain: "e-clone-ad8a2.firebaseapp.com",
+  databaseURL: "https://e-clone-ad8a2.firebaseio.com",
+  projectId: "e-clone-ad8a2",
+  storageBucket: "e-clone-ad8a2.appspot.com",
+  messagingSenderId: "759548304718",
+  appId: "1:759548304718:web:06110416509b98866c121f",
+  measurementId: "G-J5M506Z0G7"
+};
