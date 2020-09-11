afd1fd5d60873edaaf1833b7ea0563d37dc39d8b
Alvaro Alvarez <alvaro.alvarez@monokera.com>
2020-09-11 17:41:25 -0500

    set new components and config login in app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useEffect }from 'react';
 import './App.css';
 import Header from './components/header/Header';
-import Footer from './components/Footer/Footer';
+import Footer from './components/footer/Footer';
 import Home from './components/home/Home';
-import Checkout from './components/Checkout/Checkout'
+import Checkout from './components/checkout/Checkout'
+import Login from './components/login/Login';
+import { auth } from "./firebase";
+import { useStateValue } from "./stateProvider";
+
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
+
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(authUser => {
+      console.log('THE USER IS >>> ', authUser);
+
+      if (authUser) {
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      } else {
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  }, [])
+
   return (
     //BEM
     <Router>
       <div className="App">
         <Switch>
+          <Route path='/login'>
+            <Login />
+          </Route>
           <Route path='/checkout'>
             <Header />
             <Checkout/>
--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './Checkout.css'
 import Subtotal from '../subtotal/Subtotal'
+import { useStateValue } from "../../stateProvider"
+import CheckoutProduct from '../checkoutProduct/CheckoutProduct'
 
 function Checkout() {
+  const [{ basket }, dispatch] = useStateValue();
   return (
     <div className='checkout'>
       <div className="checkout__left">
@@ -11,16 +14,26 @@ function Checkout() {
           src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
           alt=""
         />
+
+        <div>
+          <h2 className="checkout__title">Your shopping Basket</h2>
+          {basket.map(item => (
+            <CheckoutProduct
+            id = {item.id}
+            title = {item.title}
+            image = {item.image}
+            price = {item.price}
+            rating = {item.rating}
+            />
+          ))}
+        </div>
       </div>
 
-      <div>
-        <h2 className="checkout__title">
-          <Subtotal/>
-          Your Shopping Basket
-        </h2>
+      <div className="checkout__right">
+        <Subtotal/>
       </div>
     </div>
-  )
+  );
 }
 
 export default Checkout
