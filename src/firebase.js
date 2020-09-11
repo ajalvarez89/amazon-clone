f12243b6add342b9ecc66b5c8eadd0d0f6dada78
Alvaro Alvarez <alvaro.alvarez@monokera.com>
2020-09-11 17:40:58 -0500

    set config for auth with firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
   apiKey: "AIzaSyBD0BSlwa10rasMMgeIdjIJqWgiudCefQA",
   authDomain: "e-clone-ad8a2.firebaseapp.com",
@@ -8,3 +10,10 @@ const firebaseConfig = {
   appId: "1:759548304718:web:06110416509b98866c121f",
   measurementId: "G-J5M506Z0G7"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
